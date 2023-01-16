a66810c583d61cea95d4e794bdf47e4179c9f026
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-01-17 00:50:37 +0530

    Create CALCULATE THE TERMS OF THE FIBONACCI SEQUENCE .C

---
+++ CALCULATE THE TERMS OF THE FIBONACCI SEQUENCE .C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+  int fib( int n);
+
+  int main(){
+
+       printf("fib %d " , fib(7));
+
+  return 0;
+
+}
+  int fib (int n){
+
+    if (n==0){
+    return 0;
+  }
+
+    if (n==1){
+    return 1;
+    }
+  int fibnm1 = fib (n-1);
+  int fibnm2 = fib (n-2);
+  int fibn = (fibnm1 + fibnm2);
+  return fibn ;
+
+  }
